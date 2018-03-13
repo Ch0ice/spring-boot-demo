ca12c67b3a136a3d4d713c85293104419bd57ba7
liuzy <liuzy@mapbar.com>
2018-03-13 18:45:19 +0800

    Initial commit

---
+++ spring-boot-docs-demo/src/main/java/com/choice/spring/demo/springbootdocsdemo/SpringBootDocsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.choice.spring.demo.springbootdocsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDocsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDocsDemoApplication.class, args);
+	}
+}
---
+++ spring-boot-kafka-demo/src/main/java/com/choice/spring/demo/springbootkafkademo/SpringBootKafkaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.choice.spring.demo.springbootkafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaDemoApplication.class, args);
+	}
+}
